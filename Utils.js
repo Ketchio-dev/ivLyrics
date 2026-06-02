7d7730eac017f8b25536311a97e1d0a0c0650b56
ivLis <me@ivl.is>
2026-06-03 00:22:30 +0900

    Update 5.3.2

--- Utils.js
+++ Utils.js
@@ -1214,7 +1214,7 @@ const Utils = {
   /**
    * Current version of the ivLyrics app
    */
-  currentVersion: "5.3.1",
+  currentVersion: "5.3.2",
 
   /**
    * Check for updates from remote repository
